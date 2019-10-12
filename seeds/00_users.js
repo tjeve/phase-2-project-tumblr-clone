9542bdd31b1edb275caa0bf700deda727f35fe52
Sue Park <suepandrice06@gmail.com>
2019-10-12 13:46:38 -0500

    updated users seed and migration files

--- seeds/00_users.js
+++ seeds/00_users.js
@@ -5,9 +5,54 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {
+          "id": 0,
+          "name": "ShirleyHopkins",
+          "userImage": "http://placehold.it/32x32",
+          "slug": "ShirleyHopkins"
+        },
+        {
+          "id": 1,
+          "name": "ShariPope",
+          "userImage": "http://placehold.it/32x32",
+          "slug": "ShariPope"
+        },
+        {
+          "id": 2,
+          "name": "HelenaBoyd",
+          "userImage": "http://placehold.it/32x32",
+          "slug": "HelenaBoyd"
+        },
+        {
+          "id": 3,
+          "name": "YoungCherry",
+          "userImage": "http://placehold.it/32x32",
+          "slug": "YoungCherry"
+        },
+        {
+          "id": 4,
+          "name": "WheelerVega",
+          "userImage": "http://placehold.it/32x32",
+          "slug": "WheelerVega"
+        },
+        {
+          "id": 5,
+          "name": "GloriaNixon",
+          "userImage": "http://placehold.it/32x32",
+          "slug": "GloriaNixon"
+        },
+        {
+          "id": 6,
+          "name": "NitaParks",
+          "userImage": "http://placehold.it/32x32",
+          "slug": "NitaParks"
+        },
+        {
+          "id": 7,
+          "name": "MavisDale",
+          "userImage": "http://placehold.it/32x32",
+          "slug": "MavisDale"
+        }
       ]);
     });
 };
