e882907e6fb47937d4221d3fbae007150e29d0ca
Terrence Eve <tijayeve@gmail.com>
2019-10-24 21:59:37 -0500

    I really don't know

--- index.js
+++ index.js
@@ -31,26 +31,6 @@ const homepageTemplate = fs.readFileSync('./templates/homepage.html', 'utf8')
 app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.get('/', function (req, res) {
-<<<<<<< HEAD
-  getAllItemsPosted()
-    // The Promise
-    .then(function (allPosts) {
-      // When the Promise is received
-      // console.log(allPosts.rows)
-      console.log('Your seed data should show up here') // console log this message
-      // res.send(allPosts.rows) // then send back the rows full of data from your database
-      res.send(
-        mustache.render(homepageTemplate, {
-          postsHTML: renderPosts(allPosts.rows)
-        })
-      ) // Mustache is working! But why is everything undefined?
-      // res.send((renderPosts(allPosts.rows))) //Wow! But why is everything undefined?
-      // res.send(allPosts.rows)
-    })
-    .catch(function () {
-      res.status(500).send('No Posts found')
-    })
-=======
 
   console.log('user information >>> ', req.user)
   if (!req.user) {
@@ -82,7 +62,6 @@ app.get('/', function (req, res) {
         res.status(500).send('No Posts found')
       })
   }
->>>>>>> 0a0b6f3cf3f1b82c5501a6379c993481d276ca0c
 })
 
 // GET Recommended posts
@@ -121,7 +100,6 @@ app.get('/users', function (request, response, next) {
 // POST new text post
 
 app.post('/posts', function (req, res) {
-<<<<<<< HEAD
   console.log(req.body, 'this is req.body')
   createPost(req.body)
     .then(function () {
@@ -154,7 +132,6 @@ app.get('/search', function (req, res) {
   res.send('got' + JSON.stringify(req.query.search) ) // sends what is entered in the search bar on the homepage to the screen.
 
 
-=======
   createPost(req.body).then(function () {
     getAllThingsPosted()
       .then(function (allPosts) {
@@ -179,7 +156,6 @@ app.post('/quotes', function (req, res) {
     .catch(function () {
       res.status(500).send('Not able to create new quote post')
     })
->>>>>>> 0a0b6f3cf3f1b82c5501a6379c993481d276ca0c
 })
 
 // --------------------------------------------------------------------------
@@ -195,13 +171,9 @@ FROM "Users"
 // `
 
 function getAllItemsPosted () {
-<<<<<<< HEAD
-  return db.raw('SELECT * FROM "Posts"')
-=======
   return db.raw(
     'SELECT "Posts".*, "Users"."userImage" FROM "Posts" LEFT JOIN "Users" On "Users"."id" = "Posts"."userId" order by "Posts"."id" desc LIMIT 20'
   )
->>>>>>> 0a0b6f3cf3f1b82c5501a6379c993481d276ca0c
 }
 
 function getAllUsers () {
@@ -209,9 +181,6 @@ function getAllUsers () {
 }
 
 function getAllThingsPosted () {
-<<<<<<< HEAD
-  return db.raw('SELECT * FROM "Posts" order by "id" desc')
-=======
   return db.raw(
     'SELECT "Posts".*, "Users"."userImage" FROM "Posts" LEFT JOIN "Users" On "Users"."id" = "Posts"."userId" order by "Posts"."id" desc LIMIT 20'
   )
@@ -219,18 +188,10 @@ function getAllThingsPosted () {
 
 function getRecommendedUsers () {
   return db.raw('SELECT * FROM "Users" ORDER BY name limit 4')
->>>>>>> 0a0b6f3cf3f1b82c5501a6379c993481d276ca0c
 }
 
 function renderPosts (post) {
   function createSinglePostHTML (postObject) {
-<<<<<<< HEAD
-    if (postObject.postedImage === null) {
-      return `
-      <div class="post-container">
-        <img src=${postObject.userImage} height="60" width="60"> 
-        
-=======
     if (postObject.postedImage !== null) {
       return `
       <div class="post-container">
@@ -261,7 +222,6 @@ function renderPosts (post) {
       <div class="post-container">
         <img src=${postObject.userImage} height="60" width="60">
 
->>>>>>> 0a0b6f3cf3f1b82c5501a6379c993481d276ca0c
         <div class="content-container">
           <h2>${postObject.title}</h2>
           ${postObject.postedMessage}
@@ -272,25 +232,6 @@ function renderPosts (post) {
     
       </div>
       `
-<<<<<<< HEAD
-    } else {
-      return `<div class="post-container">
-                <img class="user-img" src=${
-  postObject.userImage
-} height="60" width="59">
-
-                <div class="img-post-container">
-                   <img class="posted-img" src=${
-  postObject.postedImage
-} height="700" width="500">
-                   <div class="posted-message">${postObject.postedMessage}</div>
-                    <div class="post-footer">
-                      ${postObject.numberOfNotes} notes
-                    </div>
-                </div>
-              </div>`
-=======
->>>>>>> 0a0b6f3cf3f1b82c5501a6379c993481d276ca0c
     }
   }
 
@@ -306,30 +247,6 @@ function createPost (postObject) {
   )
 }
 
-<<<<<<< HEAD
-function renderRecommendedUsers (user) {
-  // function createSingleRecommendation (userObject) {
-  //   return `
-  //   <div>
-  //     <img class="recommended-user-img" src=${userObject.userImage} height="60" width="59">
-  //     <h2 class="username">${userObject.name}</h2>
-  //     <p class="tagline">${userObject.tagline}</p>
-  //   </div>
-  // `
-  // }
-}
-
-require('./src/local-auth.js')(app)
-
-// Search Function
-
-function getSearchedForContent (searchedString) {
-  return db.raw(
-    'SELECT * FROM "Posts" WHERE "postedMessage" = ?',
-    [searchedString]
-  )
-}
-=======
 function createQuotePost (postObject) {
   return db.raw('INSERT INTO "Posts" ("quote", "source") VALUES (?, ?)', [
     postObject.quote,
@@ -366,4 +283,3 @@ function renderRecommendedUsers (user) {
 require('./src/auth-local.js')(app, passport)
 require('./src/auth-facebook.js')(app, passport)
 require('./src/auth.js')(app, passport)
->>>>>>> 0a0b6f3cf3f1b82c5501a6379c993481d276ca0c
