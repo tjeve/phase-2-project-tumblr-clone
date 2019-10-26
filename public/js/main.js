c3b48f2390d1f843edb130326db63f537ad208d9
Kazue Sasatani <segakazue@gmail.com>
2019-10-26 10:24:19 -0500

    Fixed standard js error

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,16 @@
-function deletePost (id) {
-    console.log(id)
-    $.ajax({
-        url: '/posts',
-        type: 'DELETE',
-        data: {
-            id: id
-        },
-        success: function(result) {
-            location.reload()
-            // Do something with the result
-        }
-    });
-}
+/* global $, location */
 
+function deletePost (id) {
+  console.log(id)
+  $.ajax({
+    url: '/posts',
+    type: 'DELETE',
+    data: {
+      id: id
+    },
+    success: function (result) {
+      location.reload()
+      // Do something with the result
+    }
+  })
+}
